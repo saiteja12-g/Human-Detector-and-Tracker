40ed1449e88c66b91bd238f8f585240f0c17138b
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-10-17 01:18:03 -0400

    updated test cases

--- test/main.cpp
+++ test/main.cpp
@@ -2,10 +2,6 @@
 
 #include "lib.hpp"
 
-void function_not_used() {
-  dummy();
-  int notUsed = my_function1(3);
-}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,3 @@
 TEST(dummy_test, this_should_pass) {
   EXPECT_EQ(1, 1);
 }
-
-TEST(dummy_test, this_should_pass_too) {
-  EXPECT_EQ(my_function1(3), 3);
-}

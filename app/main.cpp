52812372d22e5ab968dcfcfac0f9e898e5462a97
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-10-31 10:58:29 -0400

    cpplint and cppcheck error final resolution

--- app/main.cpp
+++ app/main.cpp
@@ -6,30 +6,30 @@
 #include "./../include/pre_process.hpp"
 
 int main() {
-  std::vector<std::string> class_list;
+  std::vector<std::string> classes;
   std::ifstream ifs("../app/resources/coco.names");
   std::string line;
 
   while (getline(ifs, line)) {
-    class_list.push_back(line);
+    classes.push_back(line);
   }
 
   cv::VideoCapture cap(0);
-  cv::Mat video_frame;
+  cv::Mat instantaneous_frame;
 
   while (true) {
-    cap.read(video_frame);
+    cap.read(instantaneous_frame);
 
     cv::dnn::Net model;
     model = cv::dnn::readNet("./../app/resources/models/yolov5s.onnx");
 
     std::vector<cv::Mat> detections;
     Preprocess pre_obj;
-    detections = pre_obj.pre_process(video_frame, model);
+    detections = pre_obj.pre_process(instantaneous_frame, model);
 
     Postprocess post_obj;
     cv::Mat img =
-        post_obj.post_process(video_frame.clone(), detections, class_list);
+        post_obj.post_process(instantaneous_frame.clone(), detections, classes);
 
     cv::imshow("Output", img);
     cv::waitKey(1);
